21260bfe234cea7db20dc15319830ca9373ef40e
11reed <reed11tim@gmail.com>
2022-09-06 16:44:22 -0400

    minor changes

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,5 @@
 pub mod button;
-pub mod container;
-pub mod flex;
 pub mod typography;
 pub mod overflow;
+
+pub mod layout;
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ pub use css_style;
 
 pub mod components;
 
-pub use components::container::Container;
 pub use components::typography::Typography;
 pub use components::button::Button;
 pub use components::overflow::Overflow;
+
+pub use components::layout::container::Container;
+pub use components::layout::flex;
